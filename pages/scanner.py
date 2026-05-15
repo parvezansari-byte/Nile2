230d05ed5574701aaa2c3814a6fbe1efccee6aa7
parvezansari-byte <parvez.ansari@wealthy.in>
2026-05-16 01:19:54 +0530

    Update scanner.py

--- pages/scanner.py
+++ pages/scanner.py
@@ -1,10 +1,39 @@
 import streamlit as st
 
+from core.constants import UNIVERSE
+
+from analytics.scanner import (
+    run_scanner
+)
+
 
 def render_scanner():
 
-    st.title("Scanner")
+    st.title("Institutional Scanner")
 
     st.write(
-        "Institutional scanner engine"
+        "AI-powered market scanning engine"
     )
+
+    run_scan = st.button(
+        "Run Institutional Scan"
+    )
+
+    if run_scan:
+
+        with st.spinner(
+            "Scanning market..."
+        ):
+
+            scan_df = run_scanner(
+                UNIVERSE
+            )
+
+        st.subheader(
+            "Top Ranked Stocks"
+        )
+
+        st.dataframe(
+            scan_df,
+            use_container_width=True
+        )
